b07901d7fdadea519b38202243413712c1d63365
Ashley Blais <ablais@ablais2019.local>
2020-04-09 09:47:54 -0300

    part one and two - day one

--- aoc-day-one.go
+++ aoc-day-one.go
@@ -7,15 +7,7 @@ import (
 	"strings"
 )
 
-func main() {
-	data, fileReadErr := ioutil.ReadFile("./numbers.txt")
-
-	if fileReadErr != nil {
-		fmt.Print(fileReadErr)
-	}
-
-	values := strings.Split(string(data), "\n")
-
+func runPartOne(values []string) {
 	var sum = 0
 
 	for i := 0; i < len(values); i++ {
@@ -29,4 +21,44 @@ func main() {
 	}
 
 	fmt.Println(sum)
+}
+
+func runPartTwo(values []string) {
+	var sum = 0
+
+	for i := 0; i < len(values); i++ {
+		value, conversionErr := strconv.ParseFloat(values[i], 32)
+
+		if conversionErr != nil {
+			fmt.Print(conversionErr)
+		} else {
+			var fuelNeeded = (int(value/3) - 2)
+			sum += fuelNeeded
+
+			for fuelNeeded > 0 {
+				fuelNeeded = (int(fuelNeeded/3) - 2)
+
+				if fuelNeeded > 0 {
+					sum += fuelNeeded
+				} else {
+					fuelNeeded = 0
+				}
+			}
+		}
+	}
+
+	fmt.Println(sum)
+}
+
+func main() {
+	data, fileReadErr := ioutil.ReadFile("./numbers.txt")
+
+	if fileReadErr != nil {
+		fmt.Print(fileReadErr)
+	}
+
+	values := strings.Split(string(data), "\n")
+
+	runPartOne(values)
+	runPartTwo(values)
 }
